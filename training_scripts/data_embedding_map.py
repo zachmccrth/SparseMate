be9478330f7b1c1d53fb5a81f6f45db22fac1f9b
anoborame <zacharymccrth@gmail.com>
2025-04-23 10:41:38 -0400

    Refactor TruncatedLeela caching with save and load functionality
    Introduced methods to save and load model to/from cache, improving efficiency by reducing redundant model conversions. Added a config property and cache directory search logic to identify cached models based on configurations. This enhances reusability and reduces computational overhead.

--- training_scripts/data_embedding_map.py
+++ training_scripts/data_embedding_map.py
@@ -1,15 +1,13 @@
+import json
 from abc import ABC, abstractmethod
-from typing import List
-
-import onnx
+from typing import List, Optional, Dict, Any
 import onnx2torch
-
 import torch
-
 from leela_interp.core.leela_board import LeelaBoard
-
+from loguru import logger
 from dotenv import load_dotenv
 import os
+
 
 load_dotenv()
 
@@ -39,27 +37,73 @@ class TruncatedLeelaDataEmbeddingMap(DataEmbeddingMap):
     """
     A truncated version of the Leela Transformer Chess Model
     """
-
     def __init__(self, layers: int, device: torch.device = torch.device("cpu"), dtype = torch.float32, model_path = os.getenv("LC0_PATH"), cache_path = os.getenv("CACHE_DIR")):
         super().__init__()
         self.layers = layers
         self.device = device
         self.dtype = dtype
         self.model_path = model_path
-        self.cache_path = cache_path
+        self.CACHE_PATH =  os.path.join(cache_path, "TruncatedLeelaCache")
+        self.CONFIG_FILE_NAME = 'config.json'
+        self.SUBMODULE_FILE_NAME = 'model.pt'
+        self.CACHE_DIR = os.path.join(self.CACHE_PATH, f"{self.__class__.__name__}_{layers}")
 
-        self.submodule = self._load_model()
+        self.submodule: torch.nn.Module = self._load_model()
+
+    def _load_model(self) -> torch.nn.Module:
+        """
+        Loads a model, either by constructing one from the original ONNX graph, or a precached model
+        """
+        model = self._load_from_cache()
+        if not model:
+            original_graph: torch.fx.GraphModule = onnx2torch.convert(self.model_path)
+            model = cut_torch_graph(original_graph, f"encoder{self.layers}/ln2").to(self.device)
+            self._save_to_cache(model)
+        return model
+
+    def _save_to_cache(self, model) -> None:
+        os.makedirs(self.CACHE_DIR, exist_ok=True)
+        torch.save(model, os.path.join(self.CACHE_DIR, self.SUBMODULE_FILE_NAME))
+
+        with open(os.path.join(self.CACHE_DIR, self.CONFIG_FILE_NAME), "w") as f:
+            json.dump(self.config, f, indent=2)
 
 
-    def _load_model(self):
+    def _load_from_cache(self) -> Optional[torch.nn.Module]:
+        """
+        Searches the model cache and loads model if a match is found
+        """
+        cached_model_dir = self._search_cache()
+        if not cached_model_dir:
+            return None
 
-        cache_model = self._load_from_cache()
+        cached_model_path = os.path.join(cached_model_dir, self.SUBMODULE_FILE_NAME)
 
-        original_graph: torch.fx.GraphModule = onnx2torch.convert(self.model_path)
-        return cut_torch_graph(original_graph, f"encoder{self.layers}/ln2").to(self.device)
+        model = torch.load(cached_model_path, weights_only=False)
 
-    def _load_from_cache(self):
-        pass
+        return model
+
+    def _search_cache(self) -> Optional[str]:
+        """
+        Returns the top level directory for a previously saved model
+        """
+        if not os.path.isdir(self.CACHE_PATH):
+            logger.warning(f"{self.CACHE_PATH} does not exist! Cache is assumed to be empty")
+            return None
+
+        # os.walk returns an iterator of tuples (dirpath, dirnames, filenames)
+        for root, dirs, files in os.walk(self.CACHE_PATH):
+            for dir in dirs:
+                dir_path = os.path.join(root, dir)
+                # Check contents of each directory
+                for file in os.listdir(dir_path):
+                    if file == self.CONFIG_FILE_NAME:
+                        config_path = os.path.join(dir_path, file)
+                        with open(config_path) as f:
+                            config = json.load(f)
+                            if config == self.config:
+                                return dir_path
+        return None
 
     def convert_fen_to_input(self, fen: str) -> torch.Tensor:
         board = LeelaBoard.from_fen(fen)
@@ -73,6 +117,15 @@ class TruncatedLeelaDataEmbeddingMap(DataEmbeddingMap):
         with torch.no_grad():
             return self.submodule(data)
 
+    @property
+    def config(self) -> Dict[str, Any]:
+        return {
+            "base_model": "Lc0Model",
+            "activation_dim": 768,
+            "dtype": str(self.dtype),
+            "layers": self.layers,
+            "model_path": self.model_path,
+        }
 
 
 def cut_torch_graph(original_graph: torch.fx.GraphModule, last_node_name: str):
@@ -107,4 +160,3 @@ if __name__ == "__main__":
 
     outputs = data_embedding_map.embed_data(inputs)
     print(outputs)
-
